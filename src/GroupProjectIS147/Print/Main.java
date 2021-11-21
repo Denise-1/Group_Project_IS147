ea76acfc435d7c29fd19da34bc52065e0ffa1075
johang0128 <jgaleza28@gmail.com>
2021-11-21 18:44:26 -0500

    Updates 11/21/2021

--- src/GroupProjectIS147/Print/Main.java
+++ src/GroupProjectIS147/Print/Main.java
@@ -3,6 +3,7 @@ package GroupProjectIS147.Print;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LoginPage firstPage = new LoginPage();
+
     }
 }
