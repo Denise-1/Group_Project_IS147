91ad5f1b2789532ac50c12e04084adfc8e9a20ec
Denise Chen <78581408+Denise-1@users.noreply.github.com>
2021-11-28 23:48:57 -0500

    Create ChartPillAmount.java

---
+++ src/GroupProjectIS147/Print/ChartPillAmount.java
@@ -0,0 +1,14 @@
+package GroupProjectIS147.Print;
+
+import java.util.Scanner;
+
+public class ChartPillAmount {
+
+    public static String getPillArray() {
+        Scanner userPill = new Scanner(System.in);
+        System.out.println("Enter the number of pills you need to take: ");
+        String inputAmount = userPill.nextLine();
+
+        return inputAmount;
+    }
+} //ChartPillAmount Class End
