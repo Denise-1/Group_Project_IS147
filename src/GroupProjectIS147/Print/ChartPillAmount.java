d4d402fea5f72df05e273c5bb404e1f0eca2b0d6
Denise-1 <denisechenc@gmail.com>
2021-12-01 02:03:38 -0500

    getPillAmount

--- src/GroupProjectIS147/Print/ChartPillAmount.java
+++ src/GroupProjectIS147/Print/ChartPillAmount.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class ChartPillAmount {
 
-    public static String getPillAmount() {
-        Scanner userPill = new Scanner(System.in);
-        System.out.println("Enter the number of pills you need to take: ");
-        String inputAmount = userPill.nextLine();
+    public static String[] getPillAmount() {
+        Scanner userAmount = new Scanner(System.in);
 
-        return inputAmount;
+        System.out.println("Enter the number of pill(s) you need to take: ");
+        int inputAmount = userAmount.nextInt();
+
+        String[] pillTypeArray = new String[inputAmount];
+
+        for (int type = 0; type < inputAmount; type++) {
+            System.out.println("Enter the name of your medication " + (type + 1));
+            pillTypeArray[type] = userAmount.next();
+        }
+
+        System.out.println("Medication Name(s): ");
+        for (int medPrint = 0; medPrint < inputAmount; medPrint++) {
+            System.out.println(pillTypeArray[medPrint]);
+        }
+        return getPillAmount();
     }
-} //ChartPillAmount Class End
+}   //ChartPillAmount Class End
