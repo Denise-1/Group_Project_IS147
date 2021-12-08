f584ddce542b13bb59f9267ecdb50ee9172f79f1
Denise-1 <denisechenc@gmail.com>
2021-12-07 23:31:28 -0500

    input with switch

---
+++ src/GroupProjectIS147/Print/Input.java
@@ -0,0 +1,26 @@
+package GroupProjectIS147.Print;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void userInput () {
+
+        Scanner askUser = new Scanner(System.in);
+        System.out.println("What would you like to view: ");
+        int userInput = askUser.nextInt();
+
+        switch (userInput){
+            case 1:
+                // call object for pill amount class here
+            case 2:
+                //
+
+
+
+        }   //switch END
+
+    }
+
+    //display options (make into amethod instead of displaymoths)
+
+}
